919ecbf987430fee4dd04239d61ca891d9a21fd5
Galiant <antonijodev@gmail.com>
2018-08-26 12:54:08 +0100

    Search implemented. Work for now only in console mode.

--- src/Search.js
+++ src/Search.js
@@ -3,12 +3,24 @@ import Locations from "./Locations";
 import "./App.css";
 
 class Search extends Component {
+  state = {
+    query: "",
+    matchVenue: []
+  };
+
+  displayQuery = query => {
+    this.setState({ query }, this.showSearch);
+  };
+
   render() {
+    console.log(this.state.query);
     return (
       <div className="search">
         <input
           type="text"
           placeholder="Search bars & restaurants"
+          value={this.state.query}
+          onChange={e => this.displayQuery(e.target.value)}
           className="search-input"
         />
         {this.props.venues.map(myVenue => {
